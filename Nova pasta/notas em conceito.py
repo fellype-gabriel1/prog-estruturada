6e02dc5dca44aa68b70942442a7c25019636749c
Fellype Gabriel <166552474+fellype-gabriel1@users.noreply.github.com>
2024-04-16 14:07:30 -0300

    Update notas em conceito.py

--- Nova pasta/notas em conceito.py
+++ Nova pasta/notas em conceito.py
@@ -1,13 +1,19 @@
 nota=float(input("digite sua nota "))
+
 if nota >10:
     print("você digitou um número maior que 10,Tente novamente")
+    
 elif nota >=9:
     print("você tirou nota A")
+    
 elif nota >=7.5:
     print("você tirou nota B")
+    
 elif nota >=6:
     print("você tirou nota C")
+    
 elif nota >=4:
     print("você tirou nota D")
+    
 else:
     print("você tirou nota F")
