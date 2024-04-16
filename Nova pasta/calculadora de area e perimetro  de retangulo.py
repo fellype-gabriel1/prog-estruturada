f1f9e942f3cb0037630b82e16b5b35c5e65650b8
Fellype Gabriel <166552474+fellype-gabriel1@users.noreply.github.com>
2024-04-16 14:27:45 -0300

    Update calculadora de area e perimetro  de retangulo.py

--- Nova pasta/calculadora de area e perimetro  de retangulo.py
+++ Nova pasta/calculadora de area e perimetro  de retangulo.py
@@ -1,6 +1,9 @@
 base=float(input("digite a base do retangulo "))
+
 altura=float(input("digite a altura do retangulo "))
+
 area=(base*altura)
+
 perimetro=2*(base+altura)
 
 print("a area desse retangulo é de",area,"cm")
