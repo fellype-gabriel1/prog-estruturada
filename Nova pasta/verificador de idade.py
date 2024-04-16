0aeec77d07af4ffa6a9c0cac01666762da5b99a6
Fellype Gabriel <166552474+fellype-gabriel1@users.noreply.github.com>
2024-04-16 14:08:55 -0300

    Update verificador de idade.py

--- Nova pasta/verificador de idade.py
+++ Nova pasta/verificador de idade.py
@@ -1,9 +1,13 @@
 idade=int(input("digite sua idade "))
+
 if idade <13:
     print("você é criança")
+    
 elif idade >=13 and idade<=17:
     print("você é adolescente")
+    
 elif idade>=18 and idade<60:
     print("você é adulto")
+    
 else:
     print("você é idoso")
