bd33844698721dfe6afb847dd99c093b01b7e868
Fellype Gabriel <166552474+fellype-gabriel1@users.noreply.github.com>
2024-05-02 14:24:04 -0300

    Update calculadora de juros simples.py

--- Nova pasta/calculadora de juros simples.py
+++ Nova pasta/calculadora de juros simples.py
@@ -2,7 +2,7 @@ valor_principal=float(input("digite o valor principal "))
 
 taxa_de_juros=float(input("digite a taxa de juros "))
 
-periodo=int(input("digite o periodo de tempo "))
+periodo=int(input("digite o periodo de tempo em meses "))
 
 juros_simples=(valor_principal*taxa_de_juros*periodo)/100
 
